abf73576a3a065850438ac87241d879deb28716d
Hasrat Parwez <hasratp@aditiconsulting.com>
2024-08-22 13:21:17 +0530

    Initial commit

---
+++ src/main/java/com/bank/app/account_service/entity/Account.java
@@ -0,0 +1,41 @@
+package com.bank.app.account_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String accountNumber;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDate dateOpened;
+
+    private Long userId;            // Reference to User ID from user-service
+    private String userName;        // User's full name
+    private String email;           // User's email
+    private String phoneNumber;     // User's phone number
+
+
+
+}
+
+
+
